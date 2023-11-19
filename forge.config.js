91a8fc1546c86a2448c9492b5b2a4cc5faf952c9
ibnuhazar101 <ibnuhazar101@gmail.com>
2023-11-19 22:16:48 +0700

    update forge config

--- forge.config.js
+++ forge.config.js
@@ -6,11 +6,14 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        author: "supportseller",
+        description: "shopee power tools"
+      },
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin', 'win32'],
     },
     {
       name: '@electron-forge/maker-deb',
