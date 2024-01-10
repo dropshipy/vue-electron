25bea656d78a4f6a26b2597b4a7a65a15fd908f2
Ahmad Purnomo <purnomo.ahmad97@gmail.com>
2024-01-10 08:18:27 +0700

    feat: add formatter

---
+++ utils/format-currency.js
@@ -0,0 +1,10 @@
+const formatCurrency = (amount, locale = "id-ID", currency = "IDR") => {
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(amount);
+};
+
+export default formatCurrency;
