46bd5c6e57c820ce1956e297ea5e32ac6252667c
Ahmad Purnomo <purnomo.ahmad97@gmail.com>
2024-01-17 13:47:45 +0700

    feat: improve sidebar menu behavior

--- components/layout/sidebar.vue
+++ components/layout/sidebar.vue
@@ -4,21 +4,29 @@ import { SIDEBAR_MENU } from "./sidebar-menu";
 export default {
   data() {
     return {
-      isOpenMenu: true,
+      isDrawerExpanded: true,
       sidebarMenu: SIDEBAR_MENU,
-      activePath: this.$router.currentRoute.path,
+      openedMenu: null
     };
   },
 
   methods: {
-    isMenuActive(menu) {
-      if (!this.isOpenMenu) return true;
+    onClickMenu(menu) {
+      if (!Array.isArray(menu.children)) return;
 
-      if (menu?.children) {
-        return this.activePath.includes(menu.path);
+      if (this.openedMenu === menu.name) {
+        this.openedMenu = null;
+      } else {
+        this.openedMenu = menu.name;
       }
-      return this.activePath === menu.path;
     },
+    isMenuOpen(menu) {
+      return this.openedMenu === menu.name;
+    },
+    hasActiveSubmenu(menu) {
+      if (!Array.isArray(menu.children)) return false;
+      return menu.children.some((item) => item.path === this.$route.path);
+    }
   },
 
   computed: {
@@ -27,7 +35,7 @@ export default {
     logoUrl() {
       const appName = this.$config.appName;
 
-      if (this.isOpenMenu) {
+      if (this.isDrawerExpanded) {
         return require(`~/assets/brand/${appName}.png`);
       }
       return require(`~/assets/brand/${appName}-logo.png`);
@@ -36,12 +44,15 @@ export default {
   },
 
   watch: {
-    $route() {
-      this.activePath = this.$router.currentRoute.path;
-    },
-
-    isOpenMenu(val) {
+    isDrawerExpanded(val) {
       this.$emit('expanded', val);
+    }
+  },
+
+  mounted() {
+    const activeMenu = this.sidebarMenu.find(menu => this.hasActiveSubmenu(menu));
+    if (activeMenu) {
+      this.openedMenu = activeMenu.name;
     }
   }
 };
@@ -50,40 +61,44 @@ export default {
 <template>
   <aside
     class="fixed top-0 bottom-0 w-[290px] bg-white transition-all overflow-y-auto overflow-x-hidden shadow-sidebar z-20"
-    :class="{ 'w-[84px]': !isOpenMenu }">
+    :class="{ 'w-[84px]': !isDrawerExpanded }">
     <div class="flex items-center justify-between gap-2 px-5 py-3 sticky top-0 bg-white"
-      :class="{ 'gap-1 -mx-2': !isOpenMenu }">
-      <img :src="logoUrl" alt="logo" class="w-[120px] h-auto" :class="{ 'w-[30px] h-[30px]': !isOpenMenu }" />
+      :class="{ 'gap-1 -mx-2': !isDrawerExpanded }">
+      <img :src="logoUrl" alt="logo" class="w-[120px] h-auto" :class="{ 'w-[30px] h-[30px]': !isDrawerExpanded }" />
 
-      <button @click="isOpenMenu = !isOpenMenu" class="text-primary">
-        <Icon :name="isOpenMenu ? 'menu-collapse' : 'menu-expand'" :size="28" />
+      <button @click="isDrawerExpanded = !isDrawerExpanded" class="text-primary">
+        <Icon :name="isDrawerExpanded ? 'menu-collapse' : 'menu-expand'" :size="28" />
       </button>
     </div>
 
     <div class="mt-7 px-5 py-3">
-      <NuxtLink v-for="(menu, idx) in  sidebarMenu " :key="idx" class="cursor-pointer" :to="menu.path || ''">
+      <div v-for="(menu, idx) in  sidebarMenu" :key="idx" class="cursor-pointer w-full">
         <template v-if="typeof menu.isShown === 'function' ? menu.isShown($config) : true">
           <template v-if="menu.name !== 'divider'">
-            <div class="menu-item flex items-center justify-between rounded-lg w-full"
-              :class="{ 'p-3': isOpenMenu, 'px-2 py-3 justify-center': !isOpenMenu }">
+            <component :is="Array.isArray(menu.children) ? 'button': 'NuxtLink'" :to="menu.path || ''"
+              class="menu-item flex items-center rounded-lg w-full" :class="[
+                isDrawerExpanded ? 'p-3 justify-between' : 'px-2 py-3 justify-center',
+                { 'menu-item--active': hasActiveSubmenu(menu) },
+              ]" @click="onClickMenu(menu)">
               <div class="flex items-center space-x-2">
                 <Icon :name="menu.icon" class="text-primary" />
-                <span v-if="isOpenMenu" class="menu-item__text">
+                <span v-if="isDrawerExpanded" class="menu-item__text">
                   {{ menu.name }}
                 </span>
               </div>
 
-              <button v-if="menu.children && isOpenMenu" class="dropdown-icon transition-all">
+              <button v-if="menu.children && isDrawerExpanded" class="dropdown-icon transition-all"
+                :class="{ 'rotate-180': isMenuOpen(menu) }">
                 <Icon name="chv-down" :size="16" />
               </button>
-            </div>
+            </component>
 
-            <div v-if="menu.children && isMenuActive(menu)">
+            <div v-if="menu.children && (!isDrawerExpanded || isMenuOpen(menu))">
               <NuxtLink v-for="( submenu, submenuIdx ) in  menu.children " :key="submenuIdx" :to="submenu.path"
                 class="submenu-item flex items-center space-x-4 rounded-lg"
-                :class="{ 'ml-5 p-3': isOpenMenu, 'px-2 py-3 justify-center': !isOpenMenu }">
+                :class="{ 'ml-5 p-3': isDrawerExpanded, 'px-2 py-3 justify-center': !isDrawerExpanded }">
                 <Icon :name="submenu.icon" class="text-primary" />
-                <span v-if="isOpenMenu" class="submenu-item__text">
+                <span v-if="isDrawerExpanded" class="submenu-item__text">
                   {{ submenu.name }}
                 </span>
               </NuxtLink>
@@ -92,17 +107,12 @@ export default {
 
           <hr v-else class="mt-6 mb-3 border-gray-200" />
         </template>
-      </NuxtLink>
+      </div>
     </div>
   </aside>
 </template>
 
 <style scoped lang="postcss">
-.menu-item__text,
-.submenu-item__text {
-  @apply text-base text-dark font-normal transition-all;
-}
-
 .menu-item,
 .submenu-item {
   @apply transition-all;
@@ -110,23 +120,23 @@ export default {
   &:hover {
     @apply bg-primary bg-opacity-[4%];
   }
-}
 
-.nuxt-link-active {
-  .menu-item {
-    @apply bg-primary bg-opacity-[8%];
-
-    &__text {
-      @apply text-primary font-bold;
-    }
-
-    .dropdown-icon {
-      @apply text-primary rotate-180;
-    }
+  &__text {
+    @apply text-base text-dark font-normal transition-all;
   }
 }
 
-.nuxt-link-exact-active {
+.nuxt-link-active,
+.menu-item--active {
+  &.menu-item {
+    @apply bg-primary bg-opacity-[8%];
+  }
+
+  .dropdown-icon {
+    @apply text-primary;
+  }
+
+  .menu-item__text,
   .submenu-item__text {
     @apply text-primary font-bold;
   }
