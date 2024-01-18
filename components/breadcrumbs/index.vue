581f9ec8c821f7cc52d1c41f68e353706c379d46
Ahmad Purnomo <purnomo.ahmad97@gmail.com>
2024-01-18 14:04:10 +0700

    feat: add breadcrumbs component

---
+++ components/breadcrumbs/index.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="flex items-center gap-1">
+    <template v-for="(item, idx) in items">
+      <NuxtLink :to="item.link" class="text-primary last:text-dark2">{{ item.name }}</NuxtLink>
+      <span v-if="idx !== items.length - 1" class="text-[#66738F]">></span>
+    </template>
+  </div>
+</template>
