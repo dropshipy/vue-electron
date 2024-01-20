cd6d116a9b0f47fa27065578c44b5e325581b33b
Ahmad Purnomo <purnomo.ahmad97@gmail.com>
2024-01-20 22:27:12 +0700

    feat: compact format currency

--- utils/format-currency.js
+++ utils/format-currency.js
@@ -2,6 +2,7 @@ const formatCurrency = (amount, locale = "id-ID", currency = "IDR") => {
   return new Intl.NumberFormat(locale, {
     style: "currency",
     currency: currency,
+    notation: "compact",
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   }).format(amount);
