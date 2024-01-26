d621d6dd27e44d56e1222b62c3484c1cbadbab22
zuhriyal <ridhozuhriyal7@gmail.com>
2024-01-26 16:54:36 +0700

    clear all electron store after logout

--- components/layout/navbar.vue
+++ components/layout/navbar.vue
@@ -5,61 +5,66 @@ export default {
       isOpenSettings: false,
       isShowSubscriptionModal: false,
       isShowShopeeAccountModal: false,
-      isShowDeleteShopeeSessionModal: false
-    }
+      isShowDeleteShopeeSessionModal: false,
+    };
   },
 
   methods: {
     onClickMenuItem(type) {
       switch (type) {
-        case 'spt-account':
-          this.isShowSubscriptionModal = true
+        case "spt-account":
+          this.isShowSubscriptionModal = true;
           break;
-        case 'shopee-account':
-          this.isShowShopeeAccountModal = true
+        case "shopee-account":
+          this.isShowShopeeAccountModal = true;
           break;
-        case 'delete-shopee-session':
-          this.isShowDeleteShopeeSessionModal = true
+        case "delete-shopee-session":
+          this.isShowDeleteShopeeSessionModal = true;
           break;
       }
     },
     logout() {
-      localStorage.removeItem('user_info')
-      electronStore.delete("account-subscription")
+      localStorage.removeItem("user_info");
+      electronStore.clearAll();
       setTimeout(() => {
-        this.$router.replace('/login')
+        this.$router.replace("/login");
       }, 500);
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <template>
-  <nav class="sticky h-[72px] top-0 bg-white shadow-navbar flex items-center justify-end px-8 py-4 z-10">
-    <Button theme="tertiary" class="relative" @click.stop="isOpenSettings = !isOpenSettings">
+  <nav
+    class="sticky h-[72px] top-0 bg-white shadow-navbar flex items-center justify-end px-8 py-4 z-10"
+  >
+    <Button
+      theme="tertiary"
+      class="relative"
+      @click.stop="isOpenSettings = !isOpenSettings"
+    >
       <Icon name="setting" />
       <span>Settings</span>
 
       <Menu v-model="isOpenSettings">
-        <MenuItem v-if="!$config.hideSettingSpt" @click="onClickMenuItem('spt-account')">
-        <Icon name="profile" class="text-primary" />
-        <p class="text-sm whitespace-nowrap leading-normal">
-          Akun SPT
-        </p>
+        <MenuItem
+          v-if="!$config.hideSettingSpt"
+          @click="onClickMenuItem('spt-account')"
+        >
+          <Icon name="profile" class="text-primary" />
+          <p class="text-sm whitespace-nowrap leading-normal">Akun SPT</p>
         </MenuItem>
 
         <MenuItem @click="onClickMenuItem('shopee-account')">
-        <Icon name="sidebar/database-creator/shopee" class="text-primary" />
-        <p class="text-sm whitespace-nowrap leading-normal">
-          Akun Shopee
-        </p>
+          <Icon name="sidebar/database-creator/shopee" class="text-primary" />
+          <p class="text-sm whitespace-nowrap leading-normal">Akun Shopee</p>
         </MenuItem>
 
         <MenuItem @click="onClickMenuItem('delete-shopee-session')">
-        <Icon name="delete" class="text-primary" />
-        <p class="text-sm whitespace-nowrap leading-normal">
-          Hapus Sesi Login Shopee
-        </p>
+          <Icon name="delete" class="text-primary" />
+          <p class="text-sm whitespace-nowrap leading-normal">
+            Hapus Sesi Login Shopee
+          </p>
         </MenuItem>
       </Menu>
     </Button>
@@ -71,6 +76,8 @@ export default {
 
     <AppSettingsSubscriptionModal v-model="isShowSubscriptionModal" />
     <AppSettingsShopeeAccountModal v-model="isShowShopeeAccountModal" />
-    <AppSettingsDeleteShopeeSessionModal v-model="isShowDeleteShopeeSessionModal" />
+    <AppSettingsDeleteShopeeSessionModal
+      v-model="isShowDeleteShopeeSessionModal"
+    />
   </nav>
 </template>
