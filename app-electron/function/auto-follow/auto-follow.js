68a03ac3b977fa81c636b88cd17ae0c144aeef9c
zuhriyal <ridhozuhriyal7@gmail.com>
2024-05-25 20:12:03 +0700

    fix auto follow/unfolllow

--- app-electron/function/auto-follow/auto-follow.js
+++ app-electron/function/auto-follow/auto-follow.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer-extra");
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-puppeteer.use(StealthPlugin());
+// const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+// puppeteer.use(StealthPlugin());
 const { dialog } = require("electron");
 const ElectronStore = require("electron-store");
 const store = new ElectronStore();
@@ -8,6 +8,7 @@ const { loginShopee } = require("../login");
 const { postGetFollowers } = require("../../api/interface");
 const { postFollowUser } = require("../../api/interface");
 const { showSnackbar } = require("../../helpers/snackbar");
+const { getListFollower } = require("./get-list-follower");
 
 async function runAutoFollow({ chromePath, iteration }) {
   try {
@@ -17,29 +18,34 @@ async function runAutoFollow({ chromePath, iteration }) {
       executablePath: chromePath,
     });
     const page = await browser.newPage();
-    const requestDataList = [];
+
+    const { width, height } = await page.evaluate(() => {
+      return {
+        width: window.screen.width,
+        height: window.screen.height,
+      };
+    });
+
+    await page.setViewport({ width, height });
+    let requestDataList = null;
     await page.setRequestInterception(true);
     page.on("request", (request) => {
       if (
         request
           .url()
           .startsWith(
-            "https://shopee.co.id/api/v4/shop/get_shop_seller_show?shopid="
+            "https://seller.shopee.co.id/api/selleraccount/shop_info/"
           )
       ) {
-        const information = {
-          url: request.url(),
-          requestHeaders: request.headers(),
-        };
-        requestDataList.push(information);
+        requestDataList = request.headers();
         request.continue();
       } else {
         request.continue();
       }
     });
     await loginShopee(page, browser);
-    await page.goto(url);
-    while (requestDataList.length <= 0) {
+    await page.goto("https://seller.shopee.co.id/");
+    while (requestDataList == null) {
       await new Promise((resolve) => setTimeout(resolve, 500));
     }
     const context = {
@@ -47,6 +53,7 @@ async function runAutoFollow({ chromePath, iteration }) {
       requestDataList,
       iteration,
     };
+
     await startAutoFollow(context, browser);
   } catch (error) {
     dialog.showMessageBox({ message: error.message, buttons: ["OK"] });
@@ -57,52 +64,32 @@ async function runAutoFollow({ chromePath, iteration }) {
 async function startAutoFollow(context, browser) {
   try {
     const page = context.page;
-    // Create a URLSearchParams object
-    const params = new URLSearchParams(
-      new URL(context.requestDataList[0].url).search
-    );
-    // Get the value of the 'shopid' parameter
-    const shopId = params.get("shopid");
     const iteration = context.iteration;
-    console.log({ shopId });
-    console.log({ iteration });
-    const endpoint = `https://shopee.co.id/api/v4/pages/get_follower_list?limit=1&offset=0&shopid=${shopId}`;
-    const header = context.requestDataList[0].requestHeaders;
-    const getFollowersStatus = await postGetFollowers(endpoint, {
-      headers: header,
-    });
-    const followerStatus = getFollowersStatus.data.data;
-    //handle if followers are hidden by the shop
-    if (followerStatus.hide_follow) {
-      await page.evaluate(() => {
-        alert("Toko ini menyembunyikan pengikutnya, kamu bisa coba toko lain");
-      });
-      await browser.close();
-    } else {
-      let followingCount = 0;
-      while (followingCount < iteration) {
-        let endPointGetFollowers = `https://shopee.co.id/api/v4/pages/get_follower_list?limit=20&offset=${followingCount}&shopid=${shopId}`;
-        const getFollowers = await postGetFollowers(endPointGetFollowers, {
-          headers: header,
-        });
-        let indexFollowing = 0;
-        let dataFollowers = getFollowers.data.data.accounts;
-        while (followingCount < iteration && indexFollowing < 20) {
-          const follow = await postFollowUser(
-            "https://shopee.co.id/api/v4/pages/follow",
-            { userid: dataFollowers[indexFollowing].shopid },
-            {
-              headers: header,
-            }
-          );
-          let username = dataFollowers[indexFollowing].username;
-          await showSnackbar({ page, message: `Berhasil follow ${username}` });
-          await page.waitForTimeout(1200);
-          followingCount++;
-          indexFollowing++;
-        }
+
+    let followingCount = 0;
+    while (followingCount < iteration) {
+      const { newHeaders, listAccount } = await getListFollower(
+        context,
+        followingCount
+      );
+
+      let indexFollowing = 0;
+      while (followingCount < iteration && indexFollowing < 20) {
+        const follow = await postFollowUser(
+          "https://shopee.co.id/api/v4/pages/follow",
+          { userid: listAccount[indexFollowing].shopid },
+          {
+            headers: newHeaders,
+          }
+        );
+        let username = listAccount[indexFollowing].username;
+        await showSnackbar({ page, message: `Berhasil follow ${username}` });
+        await page.waitForTimeout(1200);
+        followingCount++;
+        indexFollowing++;
       }
     }
+
     dialog.showMessageBox({
       message: `Program Telah Selesai`,
       buttons: ["OK"],
@@ -112,4 +99,5 @@ async function startAutoFollow(context, browser) {
     console.error("Error in the main process:", error);
   }
 }
+
 module.exports = { runAutoFollow };
