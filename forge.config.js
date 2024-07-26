9f5ef81f3ecda1b7cdaf372e3c3294a0e4ef6eab
ibnuhazar101 <ibnuhazar101@gmail.com>
2024-07-26 14:40:47 +0700

    update brand and logo supportseller

--- forge.config.js
+++ forge.config.js
@@ -6,7 +6,7 @@ module.exports = {
     asar: true,
     extraResource: [join(__dirname, "resources", "chrome")],
     // icon: `./app-electron/assets/icons/logo-${process.env.APP_NAME}`,
-    icon: `./app-electron/assets/icons/logo-tiksender`, // "tiksender" || "supportseller"
+    icon: `./app-electron/assets/icons/logo-tiksender.png`, // "tiksender" || "supportseller"
   },
   rebuildConfig: {},
   makers: [
